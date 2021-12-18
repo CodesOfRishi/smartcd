50df41c7c4287aae0fb69cea93e30ba67e23ff95
Rishi Kumar <contact.rishikmr@gmail.com>
2021-12-18 22:44:13 +0530

    smartcd: `cd --cleanup` will now display the deleted directory paths.

--- smartcd.sh
+++ smartcd.sh
@@ -14,7 +14,7 @@ __smartcd__() {
 
 	# no. of unique recently visited directories smartcd to remember
 	export SMARTCD_HIST_SIZE=${SMARTCD_HIST_SIZE:-"50"}
-	export SMARTCD_VERSION="v1.4.1"
+	export SMARTCD_VERSION="v1.5.0"
 
 	# options customizations
 	export SMARTCD_CLEANUP_OPT=${SMARTCD_CLEANUP_OPT:-"--cleanup"} # option for cleanup of log file
@@ -140,16 +140,22 @@ __smartcd__() {
 	# cleanup
 	cleanup_log() {
 		local line_no="1"
-		local tmp_log=$( mktemp )
+		local valid_paths=$( mktemp )
+		local invalid_paths=$( mktemp )
 
 		while [[ ${line_no} -le ${SMARTCD_HIST_SIZE} ]]; do
 			_path=$( sed -n $line_no'p' ${recent_dir_log} )
 
-			[[ -d ${_path} ]] && echo ${_path} >> ${tmp_log}
+			if [[ -d ${_path} ]]; then echo ${_path} >> ${valid_paths}
+			else echo ${_path} >> ${invalid_paths}; fi
 			line_no=$(( ${line_no} + 1 ))
 		done
-		cp -i ${tmp_log} ${recent_dir_log}
-		rm -rf ${tmp_log}
+		cp -i ${valid_paths} ${recent_dir_log}
+		rm -rf ${valid_paths}
+
+		sed -i '/^$/d' ${invalid_paths} # remove empty/blank lines
+		[[ -s ${invalid_paths} ]] && echo "\nThe following directories got deleted from log:" && cat ${invalid_paths}
+		rm -rf ${invalid_paths}
 	}
 
 	# ---------------------------------------------------------------------------------------------------------------------
