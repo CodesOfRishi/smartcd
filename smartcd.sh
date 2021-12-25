36837fc4d2436fc59d74012716938c856c18e086
Rishi Kumar <contact.rishikmr@gmail.com>
2021-12-26 00:39:48 +0530

    smartcd: v2.2.1

--- smartcd.sh
+++ smartcd.sh
@@ -14,7 +14,7 @@ __smartcd__() {
 
 	# no. of unique recently visited directories smartcd to remember
 	export SMARTCD_HIST_SIZE=${SMARTCD_HIST_SIZE:-"50"}
-	export SMARTCD_VERSION="v2.2.0"
+	export SMARTCD_VERSION="v2.2.1"
 
 	# options customizations
 	export SMARTCD_CLEANUP_OPT=${SMARTCD_CLEANUP_OPT:-"--clean"} # option for cleanup of log file
