dc7ba93d142d812c57f936e7a1808f8dd180d2aa
Rishi Kumar <contact.rishikmr@gmail.com>
2022-01-04 22:31:52 +0530

    smartcd: (SC2053)
    Quote the rhs of `=` in `[[ ]]` to prevent glob matching.
    
    Reference:
    https://github.com/koalaman/shellcheck/wiki/SC2053

--- smartcd.sh
+++ smartcd.sh
@@ -146,7 +146,7 @@ __smartcd__() {
 
 		if [[ -z ${git_root_dir} ]]; then
 			return 1
-		elif [[ ${git_root_dir} != ${PWD} ]]; then 
+		elif [[ ${git_root_dir} != "${PWD}" ]]; then 
 			builtin cd "${git_root_dir}" && generate_recent_dir_log && \
 				if [[ -z ${piped_value} ]]; then printf '%s\n' "${PWD}"; fi
 		fi
