2365ed44df046ee91149cf8f5fa1ccab76802632
Rishi K <contact.rishikmr@gmail.com>
2022-01-10 00:02:28 +0530

    smartcd: refactor & rename `validate_fzf_preview_cmd()`

--- smartcd.sh
+++ smartcd.sh
@@ -50,12 +50,21 @@ __smartcd::validate_selected_entry() {
 	fi
 }
 
+# configure & validate SMARTCD_FZF_PREVIEW_CMD env
+__smartcd::fzf_preview() {
+	if [[ $( whereis -b exa | __smartcd::col2 ) = *exa ]]; then
+		export SMARTCD_FZF_PREVIEW_CMD=${SMARTCD_FZF_PREVIEW_CMD:-"exa -TaF -I '.git' --icons --group-directories-first --git-ignore --colour=always"}
+	elif [[ $( whereis -b tree | __smartcd::col2 ) = *tree ]]; then
+		export SMARTCD_FZF_PREVIEW_CMD=${SMARTCD_FZF_PREVIEW_CMD:-"tree -I '.git' -C -a"}
+	else export SMARTCD_FZF_PREVIEW_CMD=${SMARTCD_FZF_PREVIEW_CMD:-""}; fi
+}
+
 __smartcd::run_fzf() {
 	local query=$*
 	local select_one
 	[[ ${SMARTCD_SELECT_ONE} -eq 1 ]] && select_one="--select-1"
 
-	validate_fzf_preview_cmd
+	__smartcd::fzf_preview
 	if [[ -z ${SMARTCD_FZF_PREVIEW_CMD} ]]; then
 		fzf ${select_one} --header "${fzf_header}" --exit-0 --query="${query}"
 	else
@@ -84,16 +93,6 @@ __smartcd__() {
 	fi
 
 	# ---------------------------------------------------------------------------------------------------------------------
-
-	# configure & validate SMARTCD_FZF_PREVIEW_CMD env
-	validate_fzf_preview_cmd() {
-		if [[ $( whereis -b exa | __smartcd::col2 ) = *exa ]]; then
-			export SMARTCD_FZF_PREVIEW_CMD=${SMARTCD_FZF_PREVIEW_CMD:-"exa -TaF -I '.git' --icons --group-directories-first --git-ignore --colour=always"}
-		elif [[ $( whereis -b tree | __smartcd::col2 ) = *tree ]]; then
-			export SMARTCD_FZF_PREVIEW_CMD=${SMARTCD_FZF_PREVIEW_CMD:-"tree -I '.git' -C -a"}
-		else export SMARTCD_FZF_PREVIEW_CMD=${SMARTCD_FZF_PREVIEW_CMD:-""}; fi
-	}
-
 
 	# generate logs of recently visited dirs
 	generate_recent_dir_log() { 
