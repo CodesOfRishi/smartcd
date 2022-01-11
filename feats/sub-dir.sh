0c0b28599e1b54c5a738c343c703b65c5d435b81
Rishi K <contact.rishikmr@gmail.com>
2022-01-11 12:58:11 +0530

    refactor: move Sub-Dir feature function to a different file.
    Move Sub-Dir feature function to ./feats/sub-dir.sh
        & source it in smartcd.sh

---
+++ feats/sub-dir.sh
@@ -0,0 +1,24 @@
+# Feature: Search & traverse sub-directories
+
+__smartcd::sub_dir_hop() {
+	local path_argument=$*
+
+	local tmp_file && tmp_file=$( mktemp )
+	builtin cd ${path_argument} 2>> "${tmp_file}"
+	local exit_status=$?
+	local err_msg && err_msg=$( tail -n 1 "${tmp_file}" )
+	rm -rf "${tmp_file}"
+
+	if [[ ${exit_status} -ne 0 ]]; then 
+		if [[ $( printf '%s\n' "${err_msg}" | tr "[:upper:]" "[:lower:]" ) = *"no such file or directory"* ]]; then
+			local fzf_header && fzf_header="SmartCd: Sub-directories"
+			local selected_entry && selected_entry=$( eval "${find_sub_dir_cmd_args}" | __smartcd::run_fzf "${path_argument}" )
+			__smartcd::validate_selected_entry
+		else
+			printf '%s\n' "${err_msg}" 1>&2
+			return 1
+		fi
+	else
+		generate_recent_dir_log
+	fi
+}
--- smartcd.sh
+++ smartcd.sh
@@ -90,29 +90,6 @@ __smartcd::run_fzf() {
 # Features
 # --------
 
-__smartcd::sub_dir_hop() {
-	local path_argument=$*
-
-	local tmp_file && tmp_file=$( mktemp )
-	builtin cd ${path_argument} 2>> "${tmp_file}"
-	local exit_status=$?
-	local err_msg && err_msg=$( tail -n 1 "${tmp_file}" )
-	rm -rf "${tmp_file}"
-
-	if [[ ${exit_status} -ne 0 ]]; then 
-		if [[ $( printf '%s\n' "${err_msg}" | tr "[:upper:]" "[:lower:]" ) = *"no such file or directory"* ]]; then
-			local fzf_header && fzf_header="SmartCd: Sub-directories"
-			local selected_entry && selected_entry=$( eval "${find_sub_dir_cmd_args}" | __smartcd::run_fzf "${path_argument}" )
-			__smartcd::validate_selected_entry
-		else
-			printf '%s\n' "${err_msg}" 1>&2
-			return 1
-		fi
-	else
-		generate_recent_dir_log
-	fi
-}
-
 __smartcd::parent_dir_hop() {
 	if [[ -z $1 ]]; then
 		builtin cd .. && generate_recent_dir_log
@@ -265,6 +242,7 @@ if [[ $( whereis -b fzf | __smartcd::col2 ) = *fzf ]]; then
 		source "${SMARTCD_ROOT}"/feats/base-dir.sh
 		source "${SMARTCD_ROOT}"/feats/hist-dir.sh
 		source "${SMARTCD_ROOT}"/feats/git-root-dir.sh
+		source "${SMARTCD_ROOT}"/feats/sub-dir.sh
 
 		alias "${SMARTCD_COMMAND}"="__smartcd__"
 
