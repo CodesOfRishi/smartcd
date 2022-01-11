d0bb157b586301b818bb98139db27ba3a42615c2
Rishi K <contact.rishikmr@gmail.com>
2022-01-11 21:44:16 +0530

    refactor: move 2 fzf functions to `./tools/fzf-utilities.sh`
    Move `__smartcd::fzf_preview()` to `__smartcd::run_fzf()`
    ./tools/fzf-utilities.sh

--- smartcd.sh
+++ smartcd.sh
@@ -47,28 +47,6 @@ __smartcd::validate_selected_entry() {
 	fi
 }
 
-# configure & validate SMARTCD_FZF_PREVIEW_CMD env
-__smartcd::fzf_preview() {
-	if [[ $( whereis -b exa | __smartcd::col2 ) = *exa ]]; then
-		export SMARTCD_FZF_PREVIEW_CMD=${SMARTCD_FZF_PREVIEW_CMD:-"exa -TaF -I '.git' --icons --group-directories-first --git-ignore --colour=always"}
-	elif [[ $( whereis -b tree | __smartcd::col2 ) = *tree ]]; then
-		export SMARTCD_FZF_PREVIEW_CMD=${SMARTCD_FZF_PREVIEW_CMD:-"tree -I '.git' -C -a"}
-	else export SMARTCD_FZF_PREVIEW_CMD=${SMARTCD_FZF_PREVIEW_CMD:-""}; fi
-}
-
-__smartcd::run_fzf() {
-	local query=$*
-	local select_one
-	[[ ${SMARTCD_SELECT_ONE} -eq 1 ]] && select_one="--select-1"
-
-	__smartcd::fzf_preview
-	if [[ -z ${SMARTCD_FZF_PREVIEW_CMD} ]]; then
-		fzf ${select_one} --header "${fzf_header}" --exit-0 --query="${query}"
-	else
-		fzf ${select_one} --header "${fzf_header}" --exit-0 --query="${query}" --preview "${SMARTCD_FZF_PREVIEW_CMD} {}"
-	fi
-}
-
 __smartcd__() {
 
 	# log files
@@ -164,6 +142,9 @@ if [[ $( whereis -b fzf | __smartcd::col2 ) = *fzf ]]; then
 
 	if [[ -n ${smartcd_finder} && -n ${smartcd_grep} ]]; then
 		__smartcd::envs
+		
+		source "${SMARTCD_ROOT}"/tools/fzf-utilities.sh
+
 		source "${SMARTCD_ROOT}"/feats/base-dir.sh
 		source "${SMARTCD_ROOT}"/feats/hist-dir.sh
 		source "${SMARTCD_ROOT}"/feats/git-root-dir.sh
