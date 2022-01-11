3b43064f7ca1642f5828183e09b585a7653209e6
Rishi K <contact.rishikmr@gmail.com>
2022-01-11 12:01:50 +0530

    refactor: move Base-Dir feature functions to a different file.
    Move Base-Dir feature functions to $SMARTCD_ROOT/feats/base_srch_trvrs.sh
        & source it in smartcd.sh

---
+++ feats/base_srch_trvrs.sh
@@ -0,0 +1,36 @@
+# Feature: Base directory search & traversal.
+
+__smartcd::select_base() {
+	local fzf_header && fzf_header="Smartcd: Select a base path"
+	local selected_entry \
+		&& selected_entry=$( for _path in "${SMARTCD_BASE_PATHS[@]}"; do printf '%s\n' "${_path}"; done | __smartcd::run_fzf )
+
+	if [[ -z ${selected_entry} ]]; then
+		printf '%s\n' "No directory selected!" 1>&2
+		return 1 
+	elif [[ ! -d ${selected_entry} ]]; then
+		printf '%s\n' "Invalid directory path!" 1>&2
+		return 1
+	else
+		export SMARTCD_BASE_DIR="${selected_entry}"
+		export smartcd_manual_base_selected='1'
+		printf '%s\n' "SmartCd: Base path: ${SMARTCD_BASE_DIR}"
+	fi
+}
+
+__smartcd::base_parent_cd() {
+	if [[ -z ${SMARTCD_BASE_PATHS[*]} ]]; then
+		printf '%s\n' "ERROR: SMARTCD_BASE_PATHS env seems to be empty!" 1>&2
+		printf '%s\n' "INFO: SMARTCD_BASE_PATHS env is an array which requires at least one valid path for base directory search & traversal." 1>&2
+		return 1
+	fi
+
+	# Needs to be configured twice; once before calling __smartcd__() & another within __smartcd__()
+	# by default always set to the 1st element of $SMARTCD_BASE_PATHS
+	[[ ${smartcd_manual_base_selected} -ne 1 ]] && export SMARTCD_BASE_DIR=${SMARTCD_BASE_PATHS[*]:0:1} 
+
+	local path_argument=$*
+	local fzf_header && fzf_header="SmartCd: [${SMARTCD_BASE_DIR}]'s sub-directories"
+	local selected_entry && selected_entry=$( eval "${find_base_dir_cmd_args}" | __smartcd::run_fzf "${path_argument}" )
+	__smartcd::validate_selected_entry
+}
--- smartcd.sh
+++ smartcd.sh
@@ -87,24 +87,6 @@ __smartcd::run_fzf() {
 	fi
 }
 
-__smartcd::select_base() {
-	local fzf_header && fzf_header="Smartcd: Select a base path"
-	local selected_entry \
-		&& selected_entry=$( for _path in "${SMARTCD_BASE_PATHS[@]}"; do printf '%s\n' "${_path}"; done | __smartcd::run_fzf )
-
-	if [[ -z ${selected_entry} ]]; then
-		printf '%s\n' "No directory selected!" 1>&2
-		return 1 
-	elif [[ ! -d ${selected_entry} ]]; then
-		printf '%s\n' "Invalid directory path!" 1>&2
-		return 1
-	else
-		export SMARTCD_BASE_DIR="${selected_entry}"
-		export smartcd_manual_base_selected='1'
-		printf '%s\n' "SmartCd: Base path: ${SMARTCD_BASE_DIR}"
-	fi
-}
-
 # Features
 # --------
 
@@ -173,23 +155,6 @@ __smartcd::git_root_dir_hop() {
 		builtin cd "${git_root_dir}" && generate_recent_dir_log && \
 			if [[ -z ${piped_value} ]]; then printf '%s\n' "${PWD}"; fi
 	fi
-}
-
-__smartcd::base_parent_cd() {
-	if [[ -z ${SMARTCD_BASE_PATHS[*]} ]]; then
-		printf '%s\n' "ERROR: SMARTCD_BASE_PATHS env seems to be empty!" 1>&2
-		printf '%s\n' "INFO: SMARTCD_BASE_PATHS env is an array which requires at least one valid path for base directory search & traversal." 1>&2
-		return 1
-	fi
-
-	# Needs to be configured twice; once before calling __smartcd__() & another within __smartcd__()
-	# by default always set to the 1st element of $SMARTCD_BASE_PATHS
-	[[ ${smartcd_manual_base_selected} -ne 1 ]] && export SMARTCD_BASE_DIR=${SMARTCD_BASE_PATHS[*]:0:1} 
-
-	local path_argument=$*
-	local fzf_header && fzf_header="SmartCd: [${SMARTCD_BASE_DIR}]'s sub-directories"
-	local selected_entry && selected_entry=$( eval "${find_base_dir_cmd_args}" | __smartcd::run_fzf "${path_argument}" )
-	__smartcd::validate_selected_entry
 }
 
 __smartcd::last_dir_hop() {
@@ -333,6 +298,8 @@ if [[ $( whereis -b fzf | __smartcd::col2 ) = *fzf ]]; then
 
 	if [[ -n ${smartcd_finder} && -n ${smartcd_grep} ]]; then
 		__smartcd::envs
+		source "${SMARTCD_ROOT}"/feats/base_srch_trvrs.sh
+
 		alias "${SMARTCD_COMMAND}"="__smartcd__"
 
 		# source key bindings for __smartcd::select_base function
