353f0d720250768f45464dcdecdcc700be158033
Rishi K <contact.rishikmr@gmail.com>
2022-01-11 20:23:57 +0530

    refactor: move envs of Last-Dir to `./feats/last-dir.sh`

--- feats/last-dir.sh
+++ feats/last-dir.sh
@@ -1,5 +1,7 @@
 # Feature: Traverse to $OLDPWD.
 
+export SMARTCD_LAST_DIR_OPT=${SMARTCD_LAST_DIR_OPT-"-"} # option for moving to $OLDPWD
+
 __smartcd::last_dir_hop() {
 	builtin cd "${OLDPWD}" && generate_recent_dir_log
 }
