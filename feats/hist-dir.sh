9fc87f8e7f8db942afd2992a897d2509a1f049af
Rishi K <contact.rishikmr@gmail.com>
2022-01-12 13:19:55 +0530

    refactor: rename `SMARTCD_RECENT_DIR_LOG` to `SMARTCD_HIST_DIR_LOG` env

--- feats/hist-dir.sh
+++ feats/hist-dir.sh
@@ -8,30 +8,30 @@ export SMARTCD_HIST_OPT=${SMARTCD_HIST_OPT-"--"} # option for searching & traver
 export SMARTCD_CLEANUP_OPT=${SMARTCD_CLEANUP_OPT-"-c --clean"} # option for cleanup of log file
 
 # log file
-SMARTCD_RECENT_DIR_LOG="${SMARTCD_CONFIG_DIR}/smartcd_recent_dir.log" # stores last 50 unique visited absolute paths
+SMARTCD_HIST_DIR_LOG="${SMARTCD_CONFIG_DIR}/smartcd_recent_dir.log" # stores last 50 unique visited absolute paths
 
 __smartcd::hist_dir() {
-	if [[ ! -s ${SMARTCD_RECENT_DIR_LOG} ]]; then
+	if [[ ! -s ${SMARTCD_HIST_DIR_LOG} ]]; then
 		printf '%s\n' "No any visited directory in record !!" 1>&2
 		return 1
 	else
 		local query=$*
 		local fzf_header && fzf_header="SmartCd: Recently visited directories"
-		local selected_entry && selected_entry=$( < "${SMARTCD_RECENT_DIR_LOG}" __smartcd::run_fzf "${query}" )
+		local selected_entry && selected_entry=$( < "${SMARTCD_HIST_DIR_LOG}" __smartcd::run_fzf "${query}" )
 		__smartcd::validate_selected_entry
 	fi
 }
 
 # generate logs of recently visited dirs
 generate_recent_dir_log() { 
-	[[ -f ${SMARTCD_RECENT_DIR_LOG} ]] || touch "${SMARTCD_RECENT_DIR_LOG}"
+	[[ -f ${SMARTCD_HIST_DIR_LOG} ]] || touch "${SMARTCD_HIST_DIR_LOG}"
 
 	local tmp_log && tmp_log=$( mktemp ) # temporary file
 	printf '%s\n' "${PWD}" >| "${tmp_log}"
-	cat "${SMARTCD_RECENT_DIR_LOG}" >> "${tmp_log}"
-	awk '!seen[$0]++' "${tmp_log}" >| "${SMARTCD_RECENT_DIR_LOG}" # remove duplicates
+	cat "${SMARTCD_HIST_DIR_LOG}" >> "${tmp_log}"
+	awk '!seen[$0]++' "${tmp_log}" >| "${SMARTCD_HIST_DIR_LOG}" # remove duplicates
 	rm -f "${tmp_log}"
-	sed -i $(( SMARTCD_HIST_SIZE + 1 ))',$ d' "${SMARTCD_RECENT_DIR_LOG}" # remove lines from line no. 51 to end. (keep only last 50 unique visited paths)
+	sed -i $(( SMARTCD_HIST_SIZE + 1 ))',$ d' "${SMARTCD_HIST_DIR_LOG}" # remove lines from line no. 51 to end. (keep only last 50 unique visited paths)
 }
 
 __smartcd::clean_log() {
@@ -40,13 +40,13 @@ __smartcd::clean_log() {
 
 	printf '%s\n' "Paths to remove: "
 	while [[ ${line_no} -le ${SMARTCD_HIST_SIZE} ]]; do
-		_path=$( sed -n ${line_no}'p' "${SMARTCD_RECENT_DIR_LOG}" )
+		_path=$( sed -n ${line_no}'p' "${SMARTCD_HIST_DIR_LOG}" )
 
 		if [[ -d ${_path} ]]; then printf '%s\n' "${_path}" >> "${valid_paths}"
 		elif [[ -n ${_path} ]]; then printf '%s\n' "${_path}"; fi
 		line_no=$(( line_no + 1 ))
 	done
 	printf '\n'
-	cp -i "${valid_paths}" "${SMARTCD_RECENT_DIR_LOG}"
+	cp -i "${valid_paths}" "${SMARTCD_HIST_DIR_LOG}"
 	rm -rf "${valid_paths}"
 }
