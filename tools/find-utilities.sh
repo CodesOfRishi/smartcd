bddc4402ff01d7b583cafae33676371dd75b21ef
Rishi K <contact.rishikmr@gmail.com>
2022-01-12 23:40:31 +0530

    fix: redirect `stderr` of `find` to `/dev/null`
    Redirect `stderr` of `find` command to `/dev/null` instead of closing its file descriptor.

--- tools/find-utilities.sh
+++ tools/find-utilities.sh
@@ -5,8 +5,8 @@ if [[ ${smartcd_finder} = *fdfind || ${smartcd_finder} = *fd ]]; then
 	local find_parent_dir_cmd_args="${smartcd_finder} --exclude .git/ --search-path \${_path} -t d --max-depth=1 -H -I"
 	local find_parent_dir_root_cmd_args="${smartcd_finder} --exclude .git/ --search-path / -t d --max-depth=1 -H -I"
 else 
-	local find_base_dir_cmd_args="${smartcd_finder} \${SMARTCD_BASE_DIR} -type d ! -path '*/\.git/*' 2>&- | ${smartcd_grep} -v '\.git$'"
-	local find_sub_dir_cmd_args="${smartcd_finder} . -type d ! -path '*/\.git/*' 2>&- | ${smartcd_grep} -v '\.git$'"
-	local find_parent_dir_cmd_args="${smartcd_finder} \${_path} -maxdepth 1 -type d ! -path '*/\.git/*' 2>&-"
-	local find_parent_dir_root_cmd_args="${smartcd_finder} / -maxdepth 1 -type d ! -path '*/\.git/*' 2>&-"
+	local find_base_dir_cmd_args="${smartcd_finder} \${SMARTCD_BASE_DIR} -type d ! -path '*/\.git/*' 2> /dev/null | ${smartcd_grep} -v '\.git$'"
+	local find_sub_dir_cmd_args="${smartcd_finder} . -type d ! -path '*/\.git/*' 2> /dev/null | ${smartcd_grep} -v '\.git$'"
+	local find_parent_dir_cmd_args="${smartcd_finder} \${_path} -maxdepth 1 -type d ! -path '*/\.git/*' 2> /dev/null"
+	local find_parent_dir_root_cmd_args="${smartcd_finder} / -maxdepth 1 -type d ! -path '*/\.git/*' 2> /dev/null"
 fi
