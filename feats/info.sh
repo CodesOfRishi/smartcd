1504c5f4d674c8219fdf7002e5a33892ef693b25
Rishi K <contact.rishikmr@gmail.com>
2022-01-16 23:28:32 +0530

    fix: fix warning info statement
    `SMARTCD_CLEANUP_OPT` env is no longer being used!

--- feats/info.sh
+++ feats/info.sh
@@ -12,5 +12,5 @@ __smartcd::version_info() {
 
 __smartcd::warning_info() {
 	printf '%s\n' "WARNING: Do not try to clean the log file while piping, as it can clean it without the user's consent!" 1>&2
-	printf '%s\n' "If you want to clean the log file, then run '${SMARTCD_COMMAND} ($( printf '%s\n' "${SMARTCD_CLEANUP_OPT}" | __smartcd::col_n 1 ) | $( printf '%s\n' "${SMARTCD_CLEANUP_OPT}" | __smartcd::col_n 2 ))'" 1>&2
+	printf '%s\n' "If you want to clean the log file, then run '${SMARTCD_COMMAND} ($( printf '%s\n' "${SMARTCD_CLEAN_LOG_OPT}" | __smartcd::col_n 1 ) | $( printf '%s\n' "${SMARTCD_CLEAN_LOG_OPT}" | __smartcd::col_n 2 ))'" 1>&2
 }
