1071322054de1d4d8d3d579f5aba71de270e813b
Rishi K <contact.rishikmr@gmail.com>
2022-01-17 23:04:54 +0530

    feat: colorize warning info

--- feats/info.sh
+++ feats/info.sh
@@ -11,6 +11,10 @@ __smartcd::version_info() {
 }
 
 __smartcd::warning_info() {
-	printf '%s\n' "WARNING: Do not try to clean the log file while piping, as it can clean it without the user's consent!" 1>&2
-	printf '%s\n' "To clean the log file run either '${SMARTCD_COMMAND} $( printf '%s\n' "${SMARTCD_CLEAN_LOG_OPT}" | __smartcd::col_n 1 )' or '${SMARTCD_COMMAND} $( printf '%s\n' "${SMARTCD_CLEAN_LOG_OPT}" | __smartcd::col_n 2 )'".
+	local colr118 && colr118=$( tput setaf 118 )
+	local colr178 && colr178=$( tput setaf 178 )
+	local colr_reset && colr_reset=$( tput sgr 0 )
+
+	printf '%s\n' "${colr178}WARNING:${colr_reset} Do not try to clean the log file while piping, as it can clean it without the user's consent!" 1>&2
+	printf '%s\n' "To clean the log file run either '${colr118}${SMARTCD_COMMAND} $( printf '%s\n' "${SMARTCD_CLEAN_LOG_OPT}" | __smartcd::col_n 1 )${colr_reset}' or '${colr118}${SMARTCD_COMMAND} $( printf '%s\n' "${SMARTCD_CLEAN_LOG_OPT}" | __smartcd::col_n 2 )${colr_reset}'".
 }
